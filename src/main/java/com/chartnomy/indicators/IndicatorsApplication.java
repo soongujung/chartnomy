d4a578ecf8519433bc1becd7ead4df02594a08c1
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-04-22 21:56:04 +0900

    초기 프로젝트 파일 커밋

---
+++ src/main/java/com/chartnomy/indicators/IndicatorsApplication.java
@@ -0,0 +1,13 @@
+package com.chartnomy.indicators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicatorsApplication.class, args);
+	}
+
+}
