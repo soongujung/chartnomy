fb0906f9eb4b882b281c8f6b61579354f3553742
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-04-22 21:57:35 +0900

    Main 클래스 명 변경

--- src/main/java/com/chartnomy/indicators/MainApplication.java
+++ src/main/java/com/chartnomy/indicators/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IndicatorsApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IndicatorsApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
