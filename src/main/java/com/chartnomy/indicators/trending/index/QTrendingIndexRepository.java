b0baaa2ba9eec6dc9a9775c9ea08f511a9194782
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-04-25 21:42:57 +0900

    차트>index 지표 리포지터리

--- src/main/java/com/chartnomy/indicators/trending/index/QTrendingIndexRepository.java
+++ src/main/java/com/chartnomy/indicators/trending/index/QTrendingIndexRepository.java
@@ -1,23 +1,8 @@
 package com.chartnomy.indicators.trending.index;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import javax.persistence.EntityManager;
-import org.springframework.stereotype.Repository;
+import com.chartnomy.indicators.kospi.entity.Kospi;
+import java.util.List;
 
-/**
- * 코스피/나스닥, 환율, 금리(미국/한국) 차트를 위한 일별 데이터 조회 Repository
- */
-@Repository
-//@RequiredArgsConstructor
-public class QTrendingIndexRepository {
-
-	private final EntityManager em;
-	private final JPAQueryFactory queryFactory;
-
-//	public QTrendingIndexRepository(EntityManager em, JPAQueryFactory queryFactory){
-	public QTrendingIndexRepository(EntityManager em){
-			this.em = em;
-			this.queryFactory = new JPAQueryFactory(em);
-		}
-	}
-//	public QTrendingIndexRepository(EntityManager em, JPAQueryFactory queryFactory){
+public interface QTrendingIndexRepository {
+	public List<Kospi> selectKospi();
+}
---
+++ src/main/java/com/chartnomy/indicators/trending/index/QTrendingIndexRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.chartnomy.indicators.trending.index;
+
+import com.chartnomy.indicators.kospi.entity.Kospi;
+import com.chartnomy.indicators.kospi.entity.QKospi;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 코스피/나스닥, 환율, 금리(미국/한국) 차트를 위한 일별 데이터 조회 Repository
+ */
+@Repository
+//@RequiredArgsConstructor
+public class QTrendingIndexRepositoryImpl implements QTrendingIndexRepository {
+
+	private final EntityManager em;
+	private final JPAQueryFactory queryFactory;
+
+//	public QTrendingIndexRepository(EntityManager em, JPAQueryFactory queryFactory){
+	public QTrendingIndexRepositoryImpl(EntityManager em){
+			this.em = em;
+			this.queryFactory = new JPAQueryFactory(em);
+	}
+
+
+	@Override
+	public List<Kospi> selectKospi() {
+		List<Kospi> fetch = queryFactory.selectFrom(QKospi.kospi).fetch();
+		return fetch;
+	}
+}
