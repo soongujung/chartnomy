7541a839ead30b3ca9d777615d2a33dfaa28b0e7
sgjung <playmymind@daum.net>
2020-04-30 19:12:00 +0900

    차트 예제 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import TrendingIndex from "@/components/TrendingIndex";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'TrendingIndex',
+      component: TrendingIndex
     }
   ]
 })
