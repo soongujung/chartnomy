46b0005ad79ba334809a3d3170db5aaa0b68cc49
sgjung <playmymind@daum.net>
2020-05-05 22:46:45 +0900

    인덱스 페이지 차트 작업 (진행중)

--- frontend/src/components/TrendingIndex.vue
+++ frontend/src/components/TrendingIndex.vue
@@ -7,42 +7,110 @@
   import * as am4core from "@amcharts/amcharts4/core";
   import * as am4charts from "@amcharts/amcharts4/charts"
   import am4thems_animated from "@amcharts/amcharts4/themes/animated";
+  import api from '@/api';
+
+  import Vue from 'vue';
+  import vueMoment from 'vue-moment';
+  Vue.use(vueMoment)
 
   export default {
     name: 'TrendingIndex',
-    mounted() {
-      let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+    data(){
+      return {
+        'chartData': [],
+        'chart': {}
+      };
+    },
+    methods:{
+      getTrendingIndexResult(){
+        api.get('/web/stock/trending')
+          .then(res=>{
+            this.chartData = res.data;
+            console.log('chartData >>> ', this.chartData);
+            // this.renderChart();
+            this.backupChart();
+          })
+          .catch(err=>{
+            console.log('err >>> ', err)
+          })
+      },
+      renderChart(){
+        let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
 
-      chart.paddingRight = 20;
+        chart.paddingRight = 20;
 
-      let data = [];
-      let visits = 10;
-      for (let i = 1; i < 366; i++) {
-        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-        data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+        let data = [];
+        let visits = 10;
+
+        data = this.chartData;
+
+        data.forEach( data => {
+          let str_date = data.date;
+          let dt_date = this.$moment(str_date).format('YYYYMMDD');
+          // console.log('dt_date >>> ', dt_date);
+        })
+
+        chart.data = data;
+        this.chartData = data;
+
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
+
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "date";
+        series.dataFields.valueY = "value";
+
+        series.tooltipText = "{valueY.value}";
+        chart.cursor = new am4charts.XYCursor();
+
+        let scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        chart.scrollbarX = scrollbarX;
+
+        this.chart = chart;
+      },
+      backupChart(){
+        let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+
+        chart.paddingRight = 20;
+
+        let data = [];
+        let visits = 10;
+        for (let i = 1; i < 366; i++) {
+          visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+          data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+        }
+
+        chart.data = data;
+        this.chartData = data;
+
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
+
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "date";
+        series.dataFields.valueY = "value";
+
+        series.tooltipText = "{valueY.value}";
+        chart.cursor = new am4charts.XYCursor();
+
+        let scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        chart.scrollbarX = scrollbarX;
+
+        this.chart = chart;
       }
-
-      chart.data = data;
-
-      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-      dateAxis.renderer.grid.template.location = 0;
-
-      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      valueAxis.tooltip.disabled = true;
-      valueAxis.renderer.minWidth = 35;
-
-      let series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.dateX = "date";
-      series.dataFields.valueY = "value";
-
-      series.tooltipText = "{valueY.value}";
-      chart.cursor = new am4charts.XYCursor();
-
-      let scrollbarX = new am4charts.XYChartScrollbar();
-      scrollbarX.series.push(series);
-      chart.scrollbarX = scrollbarX;
-
-      this.chart = chart;
+    },
+    mounted() {
+      this.getTrendingIndexResult();
     },
     beforeDestroy() {
       if (this.chart) {
