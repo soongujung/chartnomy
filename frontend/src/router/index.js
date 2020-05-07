469e2b83f0db2b10011139ab1666916b8cb49e5e
sgjung <playmymind@daum.net>
2020-05-08 08:10:19 +0900

    vue router 에 history 모드 적용  : URL에 hash가 붙는 것을 방지하기 위함

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import TrendingIndex from "@/components/TrendingIndex";
+import ZingIndex from "../components/ZingIndex";
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     // {
     //   path: '/',
@@ -16,6 +18,11 @@ export default new Router({
       path: '/',
       name: 'TrendingIndex',
       component: TrendingIndex
+    },
+    {
+      path: '/zingchart',
+      name: 'ZingIndez',
+      component: ZingIndex
     }
   ]
 })
