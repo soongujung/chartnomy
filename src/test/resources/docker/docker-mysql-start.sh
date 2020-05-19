bd0b2533753405c26c60a64388def8a9d97d556f
sgjung <playmymind@daum.net>
2020-05-20 08:32:32 +0900

    테스트 용도의 mysql 컨테이너 구동 스크립트 추가

---
+++ src/test/resources/docker/docker-mysql-start.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+name_chartnomy_mysql='chartnomy-mysql'
+
+cnt_chartnomy_mysql=`docker container ls --filter name=chartnomy-mysql | wc -l`
+cnt_chartnomy_mysql=$(($cnt_chartnomy_mysql -1))
+
+if [ $cnt_chartnomy_mysql -eq 0 ]
+then
+    echo "'$name_chartnomy_mysql' 컨테이너를 구동시킵니다."
+    docker container run --rm -d -p 23306:3306 --name chartnomy-mysql -e MYSQL_ROOT_PASSWORD=1111 -e MYSQL_DATABASE=ec2_web_stockdata -d mysql:latest
+
+else
+    echo "'$name_chartnomy_mysql' 컨테이너가 존재합니다. 기존 컨테이너를 중지하고 삭제합니다."
+    docker container stop chartnomy-mysql
+    echo "\n'$name_chartnomy_mysql' 컨테이너 삭제를 완료했습니다.\n"
+
+    echo "'$name_chartnomy_mysql' 컨테이너를 구동시킵니다."
+    docker container run --rm -d -p 23306:3306 --name chartnomy-mysql -e MYSQL_ROOT_PASSWORD=1111 -e MYSQL_DATABASE=ec2_web_stockdata -d mysql:latest
+fi
