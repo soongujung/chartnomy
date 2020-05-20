66280a58fbf2530dd32d29514ff8d5da06754eab
sgjung <playmymind@daum.net>
2020-05-20 23:24:37 +0900

    mysql 도커 컨테이너 구동 스크립트 수정

--- src/test/resources/docker/docker-mysql-start.sh
+++ src/test/resources/docker/docker-mysql-start.sh
@@ -8,13 +8,24 @@ cnt_chartnomy_mysql=$(($cnt_chartnomy_mysql -1))
 if [ $cnt_chartnomy_mysql -eq 0 ]
 then
     echo "'$name_chartnomy_mysql' 컨테이너를 구동시킵니다."
-    docker container run --rm -d -p 23306:3306 --name chartnomy-mysql -v ~/env/docker/chartnomy/volumes/chartnomy-mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1111 -e MYSQL_DATABASE=ec2_web_stockdata -d mysql:latest
+    docker container run --rm -d -p 23306:3306 --name chartnomy-mysql \
+                -v ~/env/docker/chartnomy/volumes/chartnomy-mysql:/var/lib/mysql \
+                -e MYSQL_ROOT_PASSWORD=1111 \
+                -e MYSQL_DATABASE=ec2_web_stockdata \
+                -e MYSQL_USER=testuser \
+                -d mysql:latest
 
 else
     echo "'$name_chartnomy_mysql' 컨테이너가 존재합니다. 기존 컨테이너를 중지하고 삭제합니다."
     docker container stop chartnomy-mysql
+    rm -rf ~/env/docker/chartnomy/volumes/chartnomy-mysql/*
     echo "\n'$name_chartnomy_mysql' 컨테이너 삭제를 완료했습니다.\n"
 
     echo "'$name_chartnomy_mysql' 컨테이너를 구동시킵니다."
-    docker container run --rm -d -p 23306:3306 --name chartnomy-mysql -v ~/env/docker/chartnomy/volumes/chartnomy-mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1111 -e MYSQL_DATABASE=ec2_web_stockdata -d mysql:latest
+    docker container run --rm -d -p 23306:3306 --name chartnomy-mysql \
+                -v ~/env/docker/chartnomy/volumes/chartnomy-mysql:/var/lib/mysql \
+                -e MYSQL_ROOT_PASSWORD=1111 \
+                -e MYSQL_DATABASE=ec2_web_stockdata \
+                -e MYSQL_USER=testuser \
+                -d mysql:latest
 fi
