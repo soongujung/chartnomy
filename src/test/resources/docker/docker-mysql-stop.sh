dc8416254f5ce2b5a9fdcdfe3aa105ededbec30f
sgjung <playmymind@daum.net>
2020-05-27 23:02:48 +0900

    mariadb 도커 컨테이너 구동 스크립트 수정

--- src/test/resources/docker/docker-mysql-stop.sh
+++ src/test/resources/docker/docker-mysql-stop.sh
@@ -2,7 +2,7 @@
 
 name_chartnomy_mysql='chartnomy-mysql'
 
-cnt_chartnomy_mysql=`docker container ls --filter name=chartnomy-mysql | wc -l`
+cnt_chartnomy_mysql=`docker container ls --filter name=chartnomy-mariadb | wc -l`
 cnt_chartnomy_mysql=$(($cnt_chartnomy_mysql -1))
 
 if [ $cnt_chartnomy_mysql -eq 0 ]
@@ -11,7 +11,7 @@ then
 
 else
     echo "'$name_chartnomy_mysql' 컨테이너가 존재합니다. 기존 컨테이너를 중지하고 삭제합니다."
-    docker container stop chartnomy-mysql
+    docker container stop chartnomy-mariadb
     rm -rf ~/env/docker/chartnomy/volumes/chartnomy-mariadb/*
     echo "\n'$name_chartnomy_mysql' 컨테이너 삭제를 완료했습니다.\n"
 fi
