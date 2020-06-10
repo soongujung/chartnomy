7d384fd8c693810627b48b6eb64d12d3f3e6afce
sgjung <playmymind@daum.net>
2020-06-11 07:36:23 +0900

    docker)  : test 용도 docker 컨테이너 구동 스크립트 수정

--- src/test/resources/docker/docker-mysql-repl.sh
+++ src/test/resources/docker/docker-mysql-repl.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-name_chartnomy_mysql='chartnomy-mysql'
+name_chartnomy_mysql='chartnomy-mariadb'
 
-cnt_chartnomy_mysql=`docker container ls --filter name=chartnomy-mysql | wc -l`
+cnt_chartnomy_mysql=`docker container ls --filter name=chartnomy-mariadb | wc -l`
 cnt_chartnomy_mysql=$(($cnt_chartnomy_mysql -1))
 
 if [ $cnt_chartnomy_mysql -eq 0 ]
@@ -11,5 +11,5 @@ then
 
 else
     echo "'$name_chartnomy_mysql' 컨테이너의 BASH 쉘 접속을 시작합니다."
-    docker container exec -it chartnomy-mysql sh
+    docker container exec -it chartnomy-mariadb sh
 fi
