52b8896a8d9f2851acf8b175b0d6a6a3f38d83e1
sgjung <playmymind@daum.net>
2020-06-23 08:52:22 +0900

    MockMvc 테스트 코드 추가

---
+++ src/test/java/com/chartnomy/indicators/testdocker/api/web/trending/index/mockmvc/TrendingIndexControllerTest.java
@@ -0,0 +1,47 @@
+package com.chartnomy.indicators.testdocker.api.web.trending.index.mockmvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.chartnomy.indicators.api.web.trending.index.TrendingIndexController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TrendingIndexController.class)
+public class TrendingIndexControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@DisplayName("/api/web/trending/index/DATE")
+	@Test
+	public void test_get_date_series() throws Exception {
+		String from = "20200101000000";
+		String to 	= "20201231235959";
+
+		mockMvc.perform(
+					get("/api/web/trending/index/DATE")
+						.param("from", from)
+						.param("to", to)
+				)
+				.andExpect(status().isOk())
+//				.andExpect(content().string())
+				.andDo(print());
+
+	}
+
+	private LocalDateTime processDateParam(String strDate){
+		return LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+	}
+}
